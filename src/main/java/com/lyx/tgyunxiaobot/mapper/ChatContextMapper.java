dee72253f639d3860426b65e708e8b32881d1076
lyx <1829462342@qq.com>
2023-06-25 22:27:08 +0800

    1.add ChatContext to database 2.add ExceptionHandler

---
+++ src/main/java/com/lyx/tgyunxiaobot/mapper/ChatContextMapper.java
@@ -0,0 +1,18 @@
+package com.lyx.tgyunxiaobot.mapper;
+
+import com.lyx.tgyunxiaobot.model.entity.ChatContext;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lyx
+* @description 针对表【chat_context】的数据库操作Mapper
+* @createDate 2023-06-25 21:10:25
+* @Entity com.lyx.tgyunxiaobot.model.entity.ChatContext
+*/
+public interface ChatContextMapper extends BaseMapper<ChatContext> {
+
+}
+
+
+
+
